586c21bf2c095d5d11812b91e79ae4799babfe64
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-07-19 14:56:42 -0300

    feat: added experimental data

--- src/main/resources/db/migration/cleanup/afterMigrate.sql
+++ src/main/resources/db/migration/cleanup/afterMigrate.sql
@@ -7,37 +7,57 @@ delete from `stray-pets`.pet;
 set foreign_key_checks = 1;
 
 insert into location (id, latitude, longitude, cep, logradouro, bairro, cidade)
-values (1, 3.7535, 4.679521, '585612332', 'Rua Quatro', 'Alameda', 'Cajazeiras');
+values (1, -6.892701, -38.550474, '58900000', 'Rua Julio Pajeu', 'Bairro Cristo Rei', 'Cajazeiras');
 
 insert into location (id, latitude, longitude, cep, logradouro, bairro, cidade)
-values (2, 3.7535, 4.679521, '585612332', 'Rua Quatro', 'Cristo Rei', 'Cajazeiras');
+values (2, -6.892410, -38.549463, '58900000', 'Rua Sinfrônio Gonçalves Braga', 'Bairro Cristo Rei', 'Cajazeiras');
 
 insert into location (id, latitude, longitude, cep, logradouro, bairro, cidade)
-values (3, 3.7535, 4.679521, '585612332', 'Rua Juvencio', 'Golden Park', 'Cajazeiras');
+values (3, -6.893139, -38.561127, '58900000', 'Rua Padre José Tomás', 'Centro', 'Cajazeiras');
 
 insert into location (id, latitude, longitude, cep, logradouro, bairro, cidade)
-values (4, 3.7535, 4.679521, '585612332', 'Rua Quatro', 'Golden Park', 'Cajazeiras');
+values (4, -6.889673, -38.545356, '58900000', 'Rua José Leôncio da Silva', 'Loteamento Jardim Oasis', 'Cajazeiras');
 
 insert into location (id, latitude, longitude, cep, logradouro, bairro, cidade)
-values (5, 3.7535, 4.679521, '585612332', 'Rua Alameda', 'Selecta', 'Cajazeiras');
+values (5, -6.872976, -38.560661, '58900000', 'Rua Raimundo Leite Rolim Sobrinho', 'Loteamento Casas Populares', 'Cajazeiras');
 
 insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
-values (1, 'Bolinha', 'CACHORRO', 'MACHO', 'Poddle', '2023-07-20', 'Contato: (83) 99999-9999', 1);
+values (1, 'Bolinha', 'CACHORRO', 'MACHO', 'Labrador', null, '', 1);
+
+insert into pet (id, name, type, gender, breed, adoption_date, comments, missing, location_id)
+values (2, 'Peludinho', 'GATO', 'MACHO', 'Persa', null,
+        'Eu perdi o meu gato dia 19/07, se alguem encontrar por favor telefonar para: (83) 99999-9999.',
+        true, 2);
 
 insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
-values (2, 'Bolinha2', 'GATO', 'MACHO', 'Persa', '2023-07-20', 'Contato: (83) 99999-9999', 2);
+values (3, '', 'CACHORRO', null, 'CARAMELO', '2023-05-30', '', 3);
 
 insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
-values (3, 'Bolinha3', 'GATO', 'FEMEA', 'Siames', '2023-05-30', 'Contato: (83) 99999-9999', 2);
+values (4, 'Layla', 'CACHORRO', 'FEMEA', '', null, 'Possui 5 filhotes', 4);
 
 insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
-values (4, 'Bolinha4', 'CACHORRO', 'FEMEA', 'Pastor alemão', '2022-07-20', 'Contato: (83) 99999-9999', 3);
+values (5, '', 'GATO', 'FEMEA', 'Spynx', null, '', 5);
 
-insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
-values (5, 'Bolinha5', 'CACHORRO', 'MACHO', 'Poddle', '2023-07-20', 'Contato: (83) 99999-9999', 4);
-
-insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
-values (6, 'Bolinha5', 'GATO', 'FEMEA', 'Spynx', '2023-07-20', 'Contato: (83) 99999-9999', 5);
+# insert into pet (id, name, type, gender, breed, adoption_date, comments, location_id)
+# values (6, 'Bolinha5', 'GATO', 'FEMEA', 'Spynx', '2023-07-20', 'Contato: (83) 99999-9999', 5);
 
 insert into photo (id, photo_name, photo_uri, pet_id)
-values (1, 'Foto 1', 'http://aws.com.br', 1);
+values (1, 'Foto 1', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/labrador-caracteristicas-guia-racas.webp', 1);
+
+insert into photo (id, photo_name, photo_uri, pet_id)
+values (2, 'Foto 2', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/labrador-guia-racas.webp', 1);
+
+insert into photo (id, photo_name, photo_uri, pet_id)
+values (3, 'Foto 1', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/-3402.webp', 2);
+
+insert into photo (id, photo_name, photo_uri, pet_id)
+values (4, 'Foto 2', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/gato-persa.jpg', 2);
+
+insert into photo (id, photo_name, photo_uri, pet_id)
+values (5, 'Foto 1', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/2fymi85z5vo5pcl5rsnsr3xgi.webp', 3);
+
+insert into photo (id, photo_name, photo_uri, pet_id)
+values (6, 'Foto 1', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/Cachorro_caramelo.jpg', 4);
+
+insert into photo (id, photo_name, photo_uri, pet_id)
+values (7, 'Foto 1', 'https://stray-pets.s3.sa-east-1.amazonaws.com/demo-pet-photos/download.jpg', 5);
--- src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-insert into pet (id, type) values (1, "Cachorro");
